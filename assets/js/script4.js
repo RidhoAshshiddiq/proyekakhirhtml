1619331ac7053b080f5b722c9c3c3d222c589099
RidhoAshshiddiq <140096398+RidhoAshshiddiq@users.noreply.github.com>
2024-01-06 22:17:38 +0700

    Update script4.js

--- assets/js/script4.js
+++ assets/js/script4.js
@@ -1,15 +1,12 @@
 window.onload = function() {
     // Mendapatkan alamat URL halaman saat ini
     var currentURL = window.location.href;
-
     // Menemukan semua tautan dengan kelas "nav-link"
     var navLinks = document.querySelectorAll(".nav-link");
-
     // Menghapus kelas "active" dari semua tautan
     navLinks.forEach(function(link) {
         link.classList.remove("active");
     });
-
     // Menambahkan kelas "active" pada tautan yang sesuai dengan URL
     navLinks.forEach(function(link) {
         if (currentURL.includes(link.getAttribute("href"))) {
